d9fb049042a53e456f6c62034704aedeb81a488e
Ahmed Hattali <HattaliAhmed@gmail.com>
2022-03-14 21:08:59 +0100

    Deleted unnecessary files

--- app.js
+++ app.js
@@ -89,6 +89,7 @@ var k = document.querySelector(".keyboard").offsetTop;
 var h = document.querySelector(".header").offsetHeight; 
 
 if (k - h < width) { 
+    console.log(true)
 document.querySelectorAll('.tile').forEach(tile => {
     if (((k - h) / 8) < 62) {
         const x = (k - h) / 8 + 'px';
